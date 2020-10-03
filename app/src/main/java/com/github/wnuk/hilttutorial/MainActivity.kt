99debdc01825d6cc13ad4948082fbbbdae9b7e1e
bwnuk <bartekwnuk13@gmail.com>
2020-10-03 22:56:48 +0200

    modules with binds and crush cs gson

--- app/src/main/java/com/github/wnuk/hilttutorial/MainActivity.kt
+++ app/src/main/java/com/github/wnuk/hilttutorial/MainActivity.kt
@@ -2,8 +2,15 @@ package com.github.wnuk.hilttutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.gson.Gson
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.scopes.ActivityScoped
 import javax.inject.Inject
+import javax.inject.Singleton
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -19,24 +26,40 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
+@ActivityScoped
 class SomeClass
 @Inject
 constructor(
-    private val someInterfaceImpl: SomeInterface
-){
-    fun doAThing(): String{
+    private val someInterfaceImpl: SomeInterface,
+    private val gson: Gson
+) {
+    fun doAThing(): String {
         return "Look I got: ${someInterfaceImpl.getAThing()}"
     }
 }
 
 class SomeInterfaceImpl
 @Inject
-constructor(): SomeInterface {
-    override fun getAThing() : String{
+constructor() : SomeInterface {
+    override fun getAThing(): String {
         return "A Thing"
     }
 }
 
-interface SomeInterface{
+interface SomeInterface {
     fun getAThing(): String
+}
+
+@InstallIn(ApplicationComponent::class)
+@Module
+abstract class MyModule {
+    @Singleton
+    @Binds
+    abstract fun bindSomeDependency(
+        someImpl: SomeInterfaceImpl
+    ): SomeInterface
+
+    @ActivityScoped
+    @Binds
+    abstract fun bindGson(gson: Gson): Gson
 }
